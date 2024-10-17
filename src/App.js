a8f04b6980953fc8e1b3553511daaa30f96321cf
ZoomZoom24 <solversoft@yandex.ru>
2024-10-17 03:57:22 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Highlight from './components/Highlight';
+
+function App() {
+  return (
+    <div className={'wrapper'}>
+      <header className={'header'} id={'header'}>
+        <div className={'header__body _container'}>
+          <h2 className={'header__title'}>Домашнее задание «HOC»</h2>
+        </div>
+      </header>
+
+      <main className={'main'}>
+        <div className={'main__item task _container'} id={'task2'}>
+          <header className={'task__header'}>
+            <h3 className={'task__title'}>Популярное и новое</h3>
+          </header>
+          <div className={'task__body task__body_task2'}>
+            <Highlight />
+          </div>
+        </div>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
